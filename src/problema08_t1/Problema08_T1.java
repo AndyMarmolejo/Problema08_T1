d796351bee0dd6089407c2c005a1c17ca12e1693
AndyMarmolejo <142690174+AndyMarmolejo@users.noreply.github.com>
2023-09-09 13:52:47 -0500

    Add files via upload

---
+++ src/problema08_t1/Problema08_T1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema08_t1;
+
+/**
+ *
+ * @author Akyan
+ */
+public class Problema08_T1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RegistroNombre window = new  RegistroNombre();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+    
+}
